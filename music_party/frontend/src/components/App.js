6a38f5c97fead9a24f35f3db07b49f7873de9cab
alex dionicio <alexdionicio934@hotmail.com>
2020-12-03 12:20:34 -0400

    Join Room Page

--- music_party/frontend/src/components/App.js
+++ music_party/frontend/src/components/App.js
@@ -12,9 +12,8 @@ export default class App extends Component {
 
     render() {
         return(
-        <div>
+        <div className="center">
             <HomePage />
-          
         </div>
 
         ) 
