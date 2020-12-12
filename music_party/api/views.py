f8ba494b6e2a429df4128deafbdf71450a9d5f1b
alex dionicio <alexdionicio934@hotmail.com>
2020-12-12 16:12:00 -0400

    sessions and leaving rooms

--- music_party/api/views.py
+++ music_party/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
@@ -72,3 +74,28 @@ class JoinRoom(APIView):
                 return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
             return Response({'Bad Request': 'Invalid Room code'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+                self.request.session.create()
+        data = {
+            'code': self.request.session.get('room_code')
+
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) >0:
+                room = room_results[0]
+                room.delete()
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
+
--- music_party/frontend/src/components/Room.js
+++ music_party/frontend/src/components/Room.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import {Grid, Button, Typography} from '@material-ui/core'
 
 export default class Room extends Component {
     constructor(props){
@@ -11,29 +11,73 @@ export default class Room extends Component {
         }
         this.roomCode = this.props.match.params.roomCode;
         this.getRoomDetails();
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this)
     }
 
-    getRoomDetails(){
-        fetch('get-room' + '?code=' + this.roomCode).then((response) =>
-        response.json()
-        ).then((data)=>{
-            this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pause,
-                isHost: data.is_host,
+   getRoomDetails() {
+    return fetch("get-room" + "?code=" + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+      });
+  }
 
-            })
+    leaveButtonPressed() {
+        const requestOptions = {
+            method:"POST",
+            headers: {"Content-Type": "application/json"},
+        }
+        fetch('leave-room', requestOptions).then((response)=>{
+            this.props.history.push('/')
         })
+
     }
 
     render(){
         return(
-            <div>
-                <h3>{this.roomCode}</h3>
-                <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-                <p>Host: {this.state.isHost.toString()}</p>
-            </div>
+
+            <Grid container spacing={1}>
+                
+                <Grid item xs={12} align="center">
+                    <Typography variant='h6' Component='h6'>
+                        Code: {this.roomCode}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                <Typography variant='h6' Component='h6'>
+                        Votes: {this.votesToSkip}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                <Typography variant='h6' Component='h6'>
+                        Guest can Pause: {this.state.guestCanPause.toString()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                <Typography variant='h6' Component='h6'>
+                        Host: {this.state.isHost.toString()}
+                    </Typography>
+                    </Grid>       
+                    <Grid item xs={12} align="center">
+                        <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
+                            Leave Room   
+                        </Button>
+                    </Grid>     
+            
+             </Grid>
+
+         
         )
     }
 }
+
